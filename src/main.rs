54ec8203e8adf7a29ca357b4a5ef07fe2b56f08a
Eva <evapowlison@protonmail.com>
2024-09-25 15:15:08 -0400

    Basic structure for program

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,64 @@
-#![feature(lazy_cell)]
 use std::fs;
-use std::io::{self, stdout, Write};
+use std::fs::File;
+use std::io::{stdout, Write};
 use clap::Parser;
 use polars::prelude::*;
-use polars::df;
+use polars::{df};
+
+const SEP : u8 = '|' as u8;
 
 #[derive(Parser)]
 struct Args {
     input_file: String,
-    output_file: String,
+    output_file: String ,
     n_workers: u8,
 }
 
-//fn process(filename: String) {
-//
-//}
+fn process(filename: String, out: &File) -> Result<(), ()> {
+    let lazyfile = LazyCsvReader::new(filename).with_separator(SEP).finish();
+
+    match lazyfile {
+        Ok(_) => {},
+        Err(_) => { return Err(()) },
+    };
+
+
+     
+
+    Ok(())
+}
 
 fn main() {
     let args = Args::parse();
 
-    let f = fs::read_dir(args.input_file).unwrap();
-    let files = f.copied();
+    let f = fs::read_dir(&args.input_file).unwrap();
 
-    let i: i32 = 0;
+    let mut i: f32 = 1.0;
     let n: usize = f.count();
-    let mut lock = stdout().lock();
 
-    for file in files {
-        //println!("Name: {}", file.unwrap().path().display())
-        
-        let x : i32 = i / n as i32;
-        write!(lock, "\r").unwrap();
-        write!(lock, "{x}%").unwrap();
-        io::stdout().flush().unwrap();
+    println!("Processing {n} files");
+
+    let output = File::create_new(args.output_file + "out.csv").expect("Unable to create output file");
+
+    if let Ok(files) = fs::read_dir(&args.input_file) { 
+        for file in files {
+            let p = file.unwrap().path();
+            let ext = &p.clone().extension().unwrap().to_os_string();
+            let path = p.clone().as_mut_os_str().to_str().unwrap().to_string();
+
+            match ext.to_str() {
+                Some(".csv") => process(path.to_string(), &output).expect("Unable to process file {}"),
+                _ => (),
+            }
+
+            let x : i32 = ((i / n as f32) * 100.0) as i32;
+            print!("\r{x}%");
+            _ = stdout().flush();
+            i += 1.0;
+
+        }
     }
+
+    println!("");
+    println!("All done! <3");
 }
